b6a6f6e598d61e97c54e3397d19009e0ca7835b3
Tim Heck <tih99924@bethel.edu>
2019-09-04 13:20:29 -0500

    images modal added, styles added

--- src/components/Stories/StoriesPage/StoryCard.js
+++ src/components/Stories/StoriesPage/StoryCard.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CarouselProvider, Slide, Slider, Dot } from "pure-react-carousel";
-import { Card, Icon, Image, Button, Header, Modal, Container} from 'semantic-ui-react'
+import { Card, Icon, Image, Button, Header, Modal, Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import "pure-react-carousel/dist/react-carousel.es.css";
 
 class StoryCard extends Component {
 
     state = {
-        showStory: false
+        showStory: false,
+        showImages: false
+    }
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_USER' });
     }
 
     checkFeaturedImage = (image) => {
@@ -32,10 +37,40 @@ class StoryCard extends Component {
         })
     }
 
+    openImagesModal = () => {
+        this.setState({
+            showImages: true
+        })
+    }
+
+    closeImagesModal = () => {
+        this.setState({
+            showImages: false
+        })
+    }
+
+    checkTherapist = (aquatic_therapist) => {
+        if (aquatic_therapist) {
+            return (
+                <Card.Meta>
+                    <span>Aquatic Therapist: {aquatic_therapist}</span>
+                </Card.Meta>
+            );
+        }
+    }
+
+    checkSpecificTherapist = (aquatic_therapist) => {
+        if (aquatic_therapist) {
+            return (
+                <h4 className="modal-meta">Aquatic Therapist: {this.props.story.aquatic_therapist}</h4>
+            );
+        }
+    }
+
     render() {
         return (
             <>
-                <Modal open={this.state.showStory} centered={false}>
+                <Modal className="story-modal" open={this.state.showStory} centered={false}>
                     <Icon name="close" onClick={this.closeStoryModal} />
                     <Header content={this.props.story.title} />
                     {/* CarouselProvider component found at: https://codesandbox.io/s/43pv7wm6n9?from-embed */}
@@ -47,7 +82,7 @@ class StoryCard extends Component {
                         <Slider>
                             {this.props.reduxStore.images.imagesReducer.map((image, i) =>
                                 <Slide key={image.id} tag="a" index={i}>
-                                    <Image src={image.img_link} />
+                                    <Image src={image.img_link} onClick={this.openImagesModal} />
                                 </Slide>
                             )}
                         </Slider>
@@ -60,16 +95,42 @@ class StoryCard extends Component {
                         </Container>
                     </CarouselProvider>
                     <Modal.Content>
-                        <h3>{this.props.story.name}<Icon name="flag" /></h3>
-                        <h4>{this.props.story.location}</h4>
-                        <h4>{this.props.story.category}</h4>
-                        <p>{this.props.story.message}</p>
+                        <h3>{this.props.story.title}<Icon name="flag" /></h3>
+                        <h4 className="modal-meta">{this.props.story.name}</h4>
+                        <h4 className="modal-meta">{this.props.story.location}</h4>
+                        <h4 className="modal-meta">{this.props.story.category}</h4>
+                        {this.checkSpecificTherapist(this.props.story.aquatic_therapist)}
+                        <p className="modal-message">{this.props.story.message}</p>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button basic>
+                        <Button basic onClick={this.closeStoryModal}>
                             Back to Stories
                         </Button>
                     </Modal.Actions>
+                </Modal>
+                <Modal className="images-modal" open={this.state.showImages} centered={false}>
+                    {/* CarouselProvider component found at: https://codesandbox.io/s/43pv7wm6n9?from-embed */}
+                    <Icon name="close" onClick={this.closeImagesModal} />
+                    <CarouselProvider
+                        naturalSlideWidth={1}
+                        naturalSlideHeight={1}
+                        totalSlides={this.props.reduxStore.images.imagesReducer.length}
+                    >
+                        <Slider>
+                            {this.props.reduxStore.images.imagesReducer.map((image, i) =>
+                                <Slide key={image.id} tag="a" index={i}>
+                                    <Image src={image.img_link} onClick={this.viewImages} />
+                                </Slide>
+                            )}
+                        </Slider>
+                        <Container textAlign="center">
+                            <Button.Group size="mini">
+                                {[...Array(this.props.reduxStore.images.imagesReducer.length).keys()].map(slide => (
+                                    <Button as={Dot} key={slide} icon="circle" slide={slide} />
+                                ))}
+                            </Button.Group>
+                        </Container>
+                    </CarouselProvider>
                 </Modal>
                 <Card>
                     {this.checkFeaturedImage(this.props.story.img_link)}
@@ -84,12 +145,13 @@ class StoryCard extends Component {
                         <Card.Meta>
                             <span>{this.props.story.category}</span>
                         </Card.Meta>
+                        {this.checkTherapist(this.props.story.aquatic_therapist)}
                         <Card.Description>
                             {this.props.story.message}
                         </Card.Description>
                     </Card.Content>
                     <Card.Content extra>
-                        <Button basic>
+                        <Button basic onClick={this.openStoryModal}>
                             View Story
                         </Button>
                     </Card.Content>
