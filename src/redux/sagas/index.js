bc2e351aa065d2c0795f840267aec39cda7d9605
Adam Hay <heyadam66@gmail.com>
2019-09-04 15:41:26 -0500

    Changed Add Story saga and server routes to be consistent with rest of app

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,7 +4,6 @@ import registrationSaga from './registrationSaga';
 import userSaga from './userSaga';
 import storiesSaga from './storiesSaga';
 import imagesSaga from './imagesSaga';
-import addStorySaga from './addStorySaga';
 import categoriesSaga from './categoriesSaga';
 
 // rootSaga is the primary saga.
@@ -22,7 +21,6 @@ export default function* rootSaga() {
     userSaga(),
     storiesSaga(),
     imagesSaga(),
-    addStorySaga(),
     categoriesSaga(),
   ]);
 }
