bc17b5ef9cd1c86e76ba2df883a2fadf240f0ce4
Jake Knecht <Ljknecht91@gmail.com>
2019-09-04 14:12:07 -0500

    establish state, handleChangeFor func, handleStoryEditSubmit func, backButton func, stateToProps const

--- src/components/Admin/AdminEditStoryPage/AdminEditStoryView.js
+++ src/components/Admin/AdminEditStoryPage/AdminEditStoryView.js
@@ -3,12 +3,64 @@ import { connect } from 'react-redux';
 
 class AdminEditStoryView extends Component {
 
+state = {
+
+    id: this.props.reduxStore.storiesReducer.id,
+    name: this.props.reduxStore.storiesReducer.name,
+    location: '',
+    title: '',
+    aquatic_therapist: '',
+    message: '',
+    email: '',
+    category_id: '',
+    flagged: '', 
+
+}
+
+        handleChangeFor =( event, propertyToChange )=> {
+            
+            this.setState({
+                [propertyToChange]: event.target.value
+            })
+
+        console.log(`Typing in ${event.target.value} and adding new ${propertyToChange}`);
+        
+        }   
+
+        handleStoryEditSubmit = ( ) => {
+            
+            console.log(`  'Clicked submit edited details to db'  `);
+
+            window.confirm("Are you sure you wish to submit these edited details for this item?");
+
+            this.props.dispatch({ 
+                    type: 'EDIT_DETAILS', 
+                    payload: this.state 
+            });
+
+        }
+
+        backButton = () => {
+
+            window.confirm("Leave without editing item?");
+
+            alert('Headed back to the gallery!');
+
+            this.props.history.push('/gallery');
+
+        }
+
     render() {
-        return (
-            <>
-            </>
-        )
+            return(
+                <>
+                    
+                </>
+            )
     }
 }
 
-export default connect()(AdminEditStoryView);
+const stateToProps = (reduxStore) => ({
+    reduxStore
+})
+
+export default connect(stateToProps)(AdminEditStoryView);
