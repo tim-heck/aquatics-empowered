757c749cfdcd1c190925a86d5fac7c5e37e5a2b4
Adam Hay <heyadam66@gmail.com>
2019-09-04 14:25:29 -0500

    Categories and Flagged Buttons toggling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,6 +21,7 @@ import ShareStoryForm from '../Stories/ShareStoryForm/ShareStoryForm';
 import StoriesPage from '../Stories/StoriesPage/StoriesPage';
 import MobileNav from '../Menus/MobileNav/MobileNav';
 import AdminCategoriesPage from '../Admin/AdminCategoriesPage/AdminCategoriesPage';
+import AdminFlaggedList from '../Admin/AdminCategoriesPage/AdminFlaggedList';
 
 import './App.css';
 
@@ -60,11 +61,20 @@ class App extends Component {
               path="/stories"
               component={StoriesPage}
             />
+
+
+            {/* ---------TEMPORARY ADMIN ROUTES BELOW, CHANGE TO PROTECTED LATER------------ */}
             <Route
               exact
-              path="/admincategories"
+              path="/admin-categories"
               component={AdminCategoriesPage}
             />
+
+             <Route
+             exact
+             path = "/admin-flagged-list"
+             component = {AdminFlaggedList}
+             />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
