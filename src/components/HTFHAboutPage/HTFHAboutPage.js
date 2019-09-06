082bbfd2c6d280b243475336d9ea2229787f1299
Tim Heck <tih99924@bethel.edu>
2019-09-06 08:50:31 -0500

    styled buttons and fixed classNames on about pages

--- src/components/HTFHAboutPage/HTFHAboutPage.js
+++ src/components/HTFHAboutPage/HTFHAboutPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react'
 import './HTFH.css'
 
 class HTFHAboutPage extends Component {
@@ -21,7 +22,7 @@ class HTFHAboutPage extends Component {
 
                 {/* <a href="https://ssl.charityweb.net/aquaticsempowered/hottubbingforhope/" target="_blank" 
                 rel="noopener noreferrer" class="button">Donate!</a> */}
-                <a href="https://ssl.charityweb.net/aquaticsempowered/hottubbingforhope/"><button type="button">Donate</button></a>
+                <a className="ui button" href="https://ssl.charityweb.net/aquaticsempowered/hottubbingforhope/">Donate</a>
             </>
         )
     }
