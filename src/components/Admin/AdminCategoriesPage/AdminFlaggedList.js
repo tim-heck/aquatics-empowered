fa66d30c9f9908032b4200b5fddf8e6d0f386118
Adam Hay <heyadam66@gmail.com>
2019-09-11 10:33:40 -0500

    Added comments to AdminFlaggedList

--- src/components/Admin/AdminCategoriesPage/AdminFlaggedList.js
+++ src/components/Admin/AdminCategoriesPage/AdminFlaggedList.js
@@ -5,14 +5,18 @@ import './AdminPages.css';
 
 class AdminCategoriesPage extends Component {
 
+    // On component load, call a Saga that GETS all flagged stories
     componentDidMount = () => {
         this.props.dispatch({ type: 'FETCH_FLAGGED_STORIES' });
     }
 
+    // When user clicks Categories button, load the AdminCategoriesPage component
     handleCategoriesClick = (event) => {
         this.props.history.push('/admin-categories')
     }
 
+    // When user clicks edit, sends the story information to a reducer
+    // Then load the AdminEditStoryView component page
     editFlaggedStory = (story) => {
         this.props.dispatch({ type: 'EDIT_STORY', payload: story });
         this.props.history.push('/edit-story');
